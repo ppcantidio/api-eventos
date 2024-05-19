a1b02205a894b5c91f953434dcbf740cf25a5645
Pedro Cantidio <ppcantidio@gmail.com>
2024-05-19 12:36:10 -0300

    fix

--- api/db.py
+++ api/db.py
@@ -4,11 +4,10 @@ from dotenv import load_dotenv
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 load_dotenv()
-connect_args = {"check_same_thread": False}
+
 engine = create_engine(
     url=os.getenv("DATABASE_URL"),
     echo=True,
-    connect_args=connect_args,
 )
 
 
